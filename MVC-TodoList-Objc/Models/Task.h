aa5290941fe6ddc9eb72a73f65c38cc5887df1db
Tassio Rocha <=>
2020-05-30 16:15:30 -0300

    Create task model

---
+++ MVC-TodoList-Objc/Models/Task.h
@@ -0,0 +1,26 @@
+//
+//  Task.h
+//  MVC-TodoList-Objc
+//
+//  Created by Tássio Marcos Rocha on 30/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Task : NSObject
+
+@property (nullable, nonatomic, readwrite) NSString* id;
+@property (nonnull, nonatomic, readwrite) NSString* title;
+@property (nullable, nonatomic, readwrite) NSString* subTitle;
+@property (nullable, nonatomic, readwrite) NSString* createAt;
+@property (nonatomic, readwrite) BOOL* isFinished;
+
+-(void) completeTask:(NSString*) uuid: (NSString*) title: (NSString*) subTitle: (NSString *) createAt: (BOOL *) isFinished;
+-(void) initTask:(NSString *) title: (NSString *) subTitle: (BOOL *) isFinished;
+-(void) toggle;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MVC-TodoList-Objc/Models/Task.m
@@ -0,0 +1,45 @@
+//
+//  Task.m
+//  MVC-TodoList-Objc
+//
+//  Created by Tássio Marcos Rocha on 30/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+#import "Task.h"
+#import <Foundation/Foundation.h>
+
+@implementation Task
+
+- (void)completeTask:(NSString *)uuid :(NSString *)title :(NSString *)subTitle :(NSString *)createAt :(BOOL *)isFinished {
+	self.id = uuid;
+	self.title = title;
+	self.subTitle = subTitle;
+	self.createAt = createAt;
+	self.isFinished = isFinished;
+}
+
+- (void)initTask:(NSString *)title :(NSString *)subTitle :(BOOL *)isFinished {
+	NSUUID* uuid = [NSUUID UUID];
+	
+	NSDate* date = [[NSDate alloc] init];
+	NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setLocalizedDateFormatFromTemplate:@"dd.MM.yyyy"];
+	NSString* dating = [dateFormatter stringFromDate:date];
+	
+	self.id = [uuid UUIDString];
+	self.title = title;
+	self.subTitle = subTitle;
+	self.createAt = dating;
+	self.isFinished = isFinished;
+}
+
+- (void)toggle {
+	if(self.isFinished == true) {
+		self.isFinished = false;
+	} else {
+		self.isFinished = true;
+	}
+}
+
+@end
