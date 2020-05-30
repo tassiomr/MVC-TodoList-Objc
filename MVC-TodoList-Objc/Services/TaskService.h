3a28d3410498d5880828b8a2280ef33bd1c4870d
Tassio Rocha <=>
2020-05-30 16:15:19 -0300

    Create delete and create functions

---
+++ MVC-TodoList-Objc/Services/TaskService.h
@@ -0,0 +1,19 @@
+//
+//  TaskService.h
+//  MVC-TodoList-Objc
+//
+//  Created by Tássio Marcos Rocha on 30/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Task.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TaskService : NSObject
+-(void) createTask: (Task* ) task;
+-(void) deleteTask: (NSString* ) id;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MVC-TodoList-Objc/Services/TaskService.m
@@ -0,0 +1,49 @@
+//
+//  TaskService.m
+//  MVC-TodoList-Objc
+//
+//  Created by Tássio Marcos Rocha on 30/05/20.
+//  Copyright © 2020 Tássio Marcos Rocha. All rights reserved.
+//
+
+#import "Task.h"
+#import "TaskService.h"
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import <CoreData/CoreData.h>
+
+@implementation TaskService
+- (void)createTask:(Task *)task {
+	AppDelegate* delegate = [[AppDelegate alloc] init];
+	
+	NSManagedObjectContext *managedContext = [delegate persistentContainer].viewContext;
+	NSEntityDescription *taskEntity = [NSEntityDescription entityForName:@"TaskEntity" inManagedObjectContext:managedContext];
+	
+	NSManagedObject *taskRaw = [[NSManagedObject alloc] initWithEntity:taskEntity insertIntoManagedObjectContext:managedContext];
+	
+	[taskRaw setValue:task.id forKey:@"id"];
+	[taskRaw setValue:task.title forKey:@"title"];
+	[taskRaw setValue:task.subTitle forKey:@"subTitle"];
+	[taskRaw setValue:task.createAt forKey:@"createAt"];
+}
+
+- (void)deleteTask:(NSString *)id {
+	AppDelegate* delegate = [[AppDelegate alloc] init];
+	
+	NSManagedObjectContext *managedContext = [delegate persistentContainer].viewContext;
+	NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+	[fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"id = %@", id]];
+	
+	NSError *error = nil;
+	NSArray *result = [managedContext executeRequest:fetchRequest error:&error];
+	
+	if(!result) {
+		[error userInfo];
+		abort();
+	} else {
+		NSManagedObject *objDelete = result[0];
+		[managedContext deleteObject:objDelete];
+		[managedContext save:&error];
+	}
+}
+@end
